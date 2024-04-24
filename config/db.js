4b21333e7d0140cdbd6767e43a9ca7d0ff3a1b52
luânps34917 <130386979+luanhuupuannn@users.noreply.github.com>
2024-04-24 12:04:35 +0700

    Update db.js
    sua atlat

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const atlat="mongodb+srv://luan:123@cluster0.kugzap4.mongodb.net/demogame"
 const connect = async()=>{
     try{
         
-        await mongoose.connect(local,
+        await mongoose.connect(atlat,
             {
                 useNewUrlParser:true,
                 useUnifiedTopology:true
